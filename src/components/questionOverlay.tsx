bb84642022901523d727d47023b2159641dd5174
Cevin Thomas <cevin.thomas.ny@gmail.com>
2020-07-13 10:29:17 +0200

    Quiz or teach modal

---
+++ src/components/questionOverlay.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {View, StyleSheet, TouchableOpacity, Text, Dimensions, Modal} from "react-native";
+
+const QuestionOverlay = (props) => {
+    return (
+        <Modal animationType={"slide"}>
+            <View style={styles.container}>
+                    <View style={styles.questionsContainer}>
+                        <TouchableOpacity onPress={props.navigateToQuestionsFunction} style={styles.textButton}>
+                            <Text style={styles.text}>Quiz Me!</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.textButton}>
+                            <Text style={styles.text}>Teach Me!</Text>
+                        </TouchableOpacity>
+                    </View>
+            </View>
+        </Modal>
+    );
+};
+
+const {width} = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    questionsContainer: {
+        flexDirection: "row",
+        width: width,
+        marginLeft: "auto",
+        marginRight: "auto",
+        justifyContent: "center"
+    },
+    textButton: {
+        backgroundColor: "white",
+        padding: 20,
+        width: width * 0.4,
+        margin: 10,
+        shadowOffset: {width: 0, height: 0},
+        shadowColor: "#000",
+        elevation: 100,
+        shadowRadius: 5,
+        shadowOpacity: 0.1,
+        borderRadius: 10,
+    },
+    text: {
+        textAlign: "center"
+    }
+})
+
+export default QuestionOverlay;
--- src/components/subjects.tsx
+++ src/components/subjects.tsx
@@ -3,9 +3,12 @@ import {View, Text, TouchableOpacity, StyleSheet, Dimensions} from "react-native
 import GlobalStyles from "../utils/globalStyles"
 import BottomBarLogo from "./bottomBarLogo";
 import capitalizeFirstLetter from "../utils/functions";
+import QuestionOverlay from "./questionOverlay";
 const Subjects = (props) => {
 
     const [allSubjects, setAllSubjects] = useState<string[]>([]);
+    const [showModal, setShowModal] = useState<boolean>(false);
+    const [subjectToUse, setSubjectToUse] = useState<string>("");
 
     useEffect(() => {
             // TODO: Rename arrayQ
@@ -14,24 +17,31 @@ const Subjects = (props) => {
             setAllSubjects(subjectsWithCapitalFirst);
     }, [])
 
-    function navigateToProperQuestions(subject: string): void {
+    function navigateToProperQuestions(): void {
+        setShowModal(false)
 
         // TODO: Filter the correct subjects questions before sending to the questions screen.
 
-        const questionsToUse = props.route.params.categories[subject.toLowerCase()].questions;
+        const questionsToUse = props.route.params.categories[subjectToUse.toLowerCase()].questions;
 
         props.navigation.navigate("Questions", {
-            name: subject,
+            name: subjectToUse,
             questions: questionsToUse,
             id: "seo"
         });
     }
 
+    function openModalAndSetState(subjectToSet: string): void {
+        setShowModal(true);
+        setSubjectToUse(subjectToSet);
+    }
+
     return (
         <View style={styles.outerContainer}>
+            {showModal=== true ? <QuestionOverlay navigateToQuestionsFunction={navigateToProperQuestions}/> : null}
             <View style={styles.innerContainer}>
                 {allSubjects.length !== 0 ? allSubjects.map(subject => {
-                    return  <TouchableOpacity key={subject} style={styles.buttonContainer} onPress={() => navigateToProperQuestions(subject)}>
+                    return  <TouchableOpacity key={subject} style={styles.buttonContainer} onPress={() => openModalAndSetState(subject)}>
                         <Text style={styles.text}>{subject}</Text>
                     </TouchableOpacity>
                 }) : null}
