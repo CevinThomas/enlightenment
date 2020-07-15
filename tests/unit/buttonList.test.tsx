22d788d2d29fa6ea37df0371c4070cd5dbe73f1e
Cevin Thomas <cevin.thomas.ny@gmail.com>
2020-07-15 12:33:02 +0200

    Enzyme testing ButtonList to recieve a text element with correct title

--- tests/unit/buttonList.test.tsx
+++ tests/unit/buttonList.test.tsx
@@ -1,14 +1,29 @@
 import 'react-native';
 import React from 'react';
 import ButtonList from '../../src/components/buttonList';
+import {shallow} from "enzyme";
 
 import renderer from 'react-test-renderer';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {Text} from "react-native";
+
+Enzyme.configure({adapter: new Adapter()});
+
 
 test('ButtonList Component Renders', () => {
     const tree = renderer.create(
         <ButtonList/>
     ).toJSON();
     expect(tree).toMatchSnapshot();
+});
+
+test("test", () => {
+    const wrapper = shallow(
+        <ButtonList boxTitle={"Title"}/>
+    );
+    expect(wrapper.find(Text).contains("Title")).toBeTruthy();
+
 });
 
 
