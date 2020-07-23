c10d5cb381c6851a33cb3a5a7f89e489a7e55059
Cevin Thomas <cevin.thomas.ny@gmail.com>
2020-07-23 11:39:56 +0200

    Test

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -39,7 +39,7 @@ const Home = (props) => {
         <View style={styles.container}>
             <Text style={styles.mainHeading}>Welcome future Beast!</Text>
             <View style={styles.secondaryContainer}>
-                <Text style={styles.secondaryText}>This is place for you to be the fucking ultimate beast and
+                <Text style={styles.secondaryText}>testThis is place for you to be the fucking ultimate beast and
                     advance
                     your
                     skills in the shit that is
