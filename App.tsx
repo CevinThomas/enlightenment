12b9067327825c018ad69cb2d17797345ade221c
Cevin Thomas <cevin.thomas.ny@gmail.com>
2020-08-10 21:00:31 +0200

    fix import and add rn-cli config file to avoid duplicates warning

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import {default as theme} from "./custom-theme.json";
 import {default as mapping} from "./mapping.json";
 import {EvaIconsPack} from "@ui-kitten/eva-icons";
 import Amplify from 'aws-amplify';
-import config from './aws-exports';
+import config from './src/aws-exports';
 
 Amplify.configure(config);
 
