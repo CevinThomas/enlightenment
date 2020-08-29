f2aa20c6ecce81fdc127fff5a456943ea0c248b2
Cevin Thomas <cevin.thomas.ny@gmail.com>
2020-08-29 20:49:40 +0200

    Timestamp logout complete

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import "react-native-gesture-handler";
 import * as Font from 'expo-font';
 import {AppLoading} from 'expo';
@@ -9,7 +9,6 @@ import {default as mapping} from "./mapping.json";
 import {EvaIconsPack} from "@ui-kitten/eva-icons";
 import Amplify from 'aws-amplify';
 import config from './src/aws-exports';
-import {AppState} from "react-native";
 import {NavigationProvider} from "./src/contexts/navigationContext";
 import RootComponent from "./src/components/rootComponent";
 
@@ -18,33 +17,6 @@ Amplify.configure(config);
 export default function App() {
 
     const [fontLoaded, setFontLoaded] = useState(false);
-
-    useEffect(() => {
-        AppState.addEventListener("change", checkAppStatus);
-    }, []);
-
-    function checkAppStatus(activeState: string): void {
-        console.log(activeState);
-
-        if (activeState === "background") {
-            // Create a new Timestamp
-            const timeStamp = +new Date();
-            console.log(timeStamp);
-            // Store Timestamp in global state
-        }
-
-        if (activeState === "active") {
-            // Check if we have a timestamp stored in global state
-            // If we do, then compare the two timestamps
-            // Has 1h hour or more passed? Logout user and remove Timestamp in global state
-            // If not 1h passed, create new timestamp and store that new one in global state
-        }
-
-    }
-
-    function createTimeStamp() {
-
-    }
 
     const fetchFonts = () => {
         return Font.loadAsync({
--- src/components/rootComponent.tsx
+++ src/components/rootComponent.tsx
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import {AuthStackNavigator, BaseNavigator} from "../stackNavigators";
-import {useNavigation} from "../contexts/navigationContext";
+import {useNavigation, useNavigationUpdate} from "../contexts/navigationContext";
+import {Alert, AppState} from "react-native";
+import {Auth} from "aws-amplify";
 
 const RootComponent = () => {
 
     const navigation = useNavigation();
+    const updateNavigation = useNavigationUpdate();
+
+    const timeStampState = useRef(0);
+
+    useEffect(() => {
+        if (navigation === true) {
+            AppState.addEventListener("change", checkAppStatus);
+        } else {
+            AppState.removeEventListener("change", checkAppStatus);
+        }
+
+    }, [navigation]);
+
+    function checkAppStatus(activeState: string): void {
+
+        if (activeState === "background") {
+            const temporaryTimeStamp = +new Date();
+            timeStampState.current = temporaryTimeStamp;
+        }
+
+        if (activeState === "active") {
+            const temporaryTimeStamp = +new Date();
+            const timestampDifferenceInMs = temporaryTimeStamp - timeStampState.current;
+            const timestampDifferenceInMinutes = Math.round(((timestampDifferenceInMs % 86400000) % 3600000) / 60000);
+
+            if (timeStampState.current !== 0) {
+                if (timestampDifferenceInMinutes > 1) {
+                    Auth.signOut().then(() => {
+                        updateNavigation(false);
+                        Alert.alert("Please login again.");
+                    });
+                } else {
+                    timeStampState.current = 0;
+                }
+            }
+        }
+
+    }
 
     return (
         <>
