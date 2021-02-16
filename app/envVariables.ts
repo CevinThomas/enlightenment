813907d8066281e98cd84799f2fca3fecf381f1d
Cevin Thomas <cevin.thomas.ny@gmail.com>
2021-02-16 07:06:06 +0100

    feat: change ip address

--- app/envVariables.ts
+++ app/envVariables.ts
@@ -1,26 +1,26 @@
 const EnvVariables = {
   API_ENDPOINTS: {
-    LOGIN: "http://192.168.200.54:4000/login",
-    REGISTER: "http://192.168.200.54:4000/register",
-    GETCATEGORIES: "http://192.168.200.54:4000/questions/allCategories",
-    GETCATEGORY: "http://192.168.200.54:4000/questions?category=",
+    LOGIN: "http://192.168.200.55:4000/login",
+    REGISTER: "http://192.168.200.55:4000/register",
+    GETCATEGORIES: "http://192.168.200.55:4000/questions/allCategories",
+    GETCATEGORY: "http://192.168.200.55:4000/questions?category=",
     GETGROUPSBYCATEGORY:
-      "http://192.168.200.54:4000/groups/getGroupsByCategory",
+      "http://192.168.200.55:4000/groups/getGroupsByCategory",
     GETCATEGORIESBYSUBJECTCHOSEN:
-      "http://192.168.200.54:4000/categories/getCategoriesBySubject",
-    GETQUESTIONSBYGROUPNAME: "http://192.168.200.54:4000/questions/group",
-    GETQUESTIONSBYGROUPID: "http://192.168.200.54:4000/questions/groupId",
+      "http://192.168.200.55:4000/categories/getCategoriesBySubject",
+    GETQUESTIONSBYGROUPNAME: "http://192.168.200.55:4000/questions/group",
+    GETQUESTIONSBYGROUPID: "http://192.168.200.55:4000/questions/groupId",
     GETCATEGORIESBYLICENCE:
-      "http://192.168.200.54:4000/questions/allCategoriesByLicence",
-    GETAREABYLICENCE: "http://192.168.200.54:4000/areas/getAreasByLicence",
+      "http://192.168.200.55:4000/questions/allCategoriesByLicence",
+    GETAREABYLICENCE: "http://192.168.200.55:4000/areas/getAreasByLicence",
     GETSUBJECTSBYAREACHOSEN:
-      "http://192.168.200.54:4000/questions/allSubjectsByArea",
-    SAVERESULTS: "http://192.168.200.54:4000/results/save",
-    GETINVITES: "http://192.168.200.54:4000/invites/get",
-    HANDLEINVITES: "http://192.168.200.54:4000/user/handleInvite",
-    GETRESULTS: "http://192.168.200.54:4000/results/get",
-    DELETERESULTS: "http://192.168.200.54:4000/results/delete",
-    GETGROUPINFO: "http://192.168.200.54:4000/groups/getGroupInfo",
+      "http://192.168.200.55:4000/questions/allSubjectsByArea",
+    SAVERESULTS: "http://192.168.200.55:4000/results/save",
+    GETINVITES: "http://192.168.200.55:4000/invites/get",
+    HANDLEINVITES: "http://192.168.200.55:4000/user/handleInvite",
+    GETRESULTS: "http://192.168.200.55:4000/results/get",
+    DELETERESULTS: "http://192.168.200.55:4000/results/delete",
+    GETGROUPINFO: "http://192.168.200.55:4000/groups/getGroupInfo",
   },
 };
 
